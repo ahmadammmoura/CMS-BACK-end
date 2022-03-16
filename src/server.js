5c6dc11c995f5f56d54c1815c7a2f006124541dd
ahmad ammoura <ammoura575@gmail.com>
2022-03-16 11:28:38 +0300

    done

--- src/server.js
+++ src/server.js
@@ -14,7 +14,11 @@ const customerRouter = require('./routes/customer.route')
 
 /////// Data base conncetion //////
 const { DBconction } = require("./DBconction");
+const res = require("express/lib/response");
 DBconction();
+app.use('/test',(req,res)=>{
+  res.send("Ddddddddddddddddd")
+});
 
 app.use('/',AuthRouter);
 app.use(`/admin`, adminRouter);
